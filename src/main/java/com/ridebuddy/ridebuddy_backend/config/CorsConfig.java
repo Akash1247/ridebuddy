f0c0528cb50351b18781cb872a5df8575cf60749
Akash1247 <ay99059@gmail.com>
2026-06-12 03:09:19 +0530

    Fix Vercel CORS

--- src/main/java/com/ridebuddy/ridebuddy_backend/config/CorsConfig.java
+++ src/main/java/com/ridebuddy/ridebuddy_backend/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://ridebuddy-nine.vercel.app"
+                "https://*.vercel.app"
         ));
 
         configuration.setAllowedMethods(
