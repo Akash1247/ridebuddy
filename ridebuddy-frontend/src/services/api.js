916c9d0d667fdeb60d97e8250a0afa57ab552a0e
Akash1247 <96951564+Akash1247@users.noreply.github.com>
2026-06-16 11:00:48 +0530

    Update API base URL to Render deployment

--- ridebuddy-frontend/src/services/api.js
+++ ridebuddy-frontend/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://localhost:8080/api"
-});
-
 // const api = axios.create({
-//     // Localhost hata kar Render ka URL daal diya
-//     baseURL: "https://ridebuddy-zhsv.onrender.com/api" 
+//     baseURL: "http://localhost:8080/api"
 // });
+
+const api = axios.create({
+    // Localhost hata kar Render ka URL daal diya
+    baseURL: "https://ridebuddy-zhsv.onrender.com/api" 
+});
 
 api.interceptors.request.use((config) => {
 
